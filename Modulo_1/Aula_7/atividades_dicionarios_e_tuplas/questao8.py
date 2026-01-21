42a35437dafc3d45daa3979b9b511b4e7c200bf0
Mateus Sebastian <mateussebastian1@gmail.com>
2026-01-21 08:30:26 -0300

    correção de type check da questão 8

--- Modulo_1/Aula_7/atividades_dicionarios_e_tuplas/questao8.py
+++ Modulo_1/Aula_7/atividades_dicionarios_e_tuplas/questao8.py
@@ -4,11 +4,8 @@ pontos_turisticos = {
     (-7.2034, -39.3206): 'Açude Velho'
 }
 
-coordenadas = (input("Digite as coordenadas (latitude, longitude): ").split(','))
+coordenadas = [float(x) for x in input("Digite as coordenadas (latitude, longitude): ").split(',')]
 
-for i in range(len(coordenadas)):
-    coordenadas[i] = float(coordenadas[i])
-    
 for i in pontos_turisticos.keys():
     if coordenadas == list(i):
         print(f'Ponto turístico: {pontos_turisticos[i]}')
